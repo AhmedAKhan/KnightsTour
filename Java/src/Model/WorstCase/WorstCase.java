39e64b59477aa41c4083640952219ef9b5eba9ba
AhmedAKhan <khanaahmed10@gmail.com>
2015-04-15 13:41:10 -0400

    added a position class, get next turn function, is in bounds function, and started the find path function

--- Java/src/Model/WorstCase/WorstCase.java
+++ Java/src/Model/WorstCase/WorstCase.java
@@ -13,10 +13,49 @@ public class WorstCase implements Model{
 
     private void findPath(){
 
-        
+    }
+    private void findPath(int row, int col, int totalTurns){
 
+        for(int turnsInSpot = 0; turnsInSpot < 8; turnsInSpot++){
+            //get next position
+            Position currentPosition = getPositionAt(row, col, turnsInSpot);
+
+            //check if position is out of bounds
+            if(!isInBounds(currentPosition)) continue;
+            //check if is empty
+            if(positions[currentPosition.row][currentPosition.col] != 0) continue;
+
+            positions[currentPosition.row][currentPosition.col] = totalTurns+1;
+
+            //base case
+            if(totalTurns >= positions.length*positions[0].length) return;
+        }
+    }
+    private boolean isInBounds(Position position){
+        if(position.row < 0 || position.col < 0) return false;
+        if(position.row > positions.length || position.col > positions[position.row].length) return false;
+        return true;
     }
 
+    class Position{
+        private int row;
+        private int col;
+        public Position(int r, int c){row = r; col = c;}
+    }
+    private Position getPositionAt(int row, int col, int c){
+        switch (c){
+            case 0: return new Position(row+2, col+1);
+            case 1: return new Position(row+2, col-1);
+            case 2: return new Position(row-2, col+1);
+            case 3: return new Position(row-2, col-1);
+            case 4: return new Position(row+1, col+2);
+            case 5: return new Position(row-1, col+2);
+            case 6: return new Position(row+1, col-2);
+            case 7: return new Position(row-1, col-2);
+        }
+        System.out.println("c is out of bounds in the getPositionAtTile function returning a c of 7");
+        return new Position(row, col);
+    }
 
     @Override
     public int getValueAtPosition(int row, int col) {
